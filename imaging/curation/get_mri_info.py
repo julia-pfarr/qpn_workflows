1978f45e5b6de9d7f641cc97fb50e2907e1b127d
nikhil153 <nikhil153@gmail.com>
2024-11-20 17:19:55 -0500

    ported qpn data workflow scripts to new repo for public access

---
+++ imaging/curation/get_mri_info.py
@@ -0,0 +1,106 @@
+import argparse
+import json
+import os
+from pathlib import Path
+
+import nipoppy.workflow.logger as my_logger
+import pandas as pd
+from nipoppy.workflow.dicom_org.utils import check_valid_dicom, search_dicoms
+from nipoppy.workflow.utils import (COL_ORG_STATUS, DNAME_BACKUPS_DOUGHNUT,
+                                    FNAME_DOUGHNUT, load_doughnut,
+                                    participant_id_to_dicom_id, save_backup,
+                                    session_id_to_bids_session)
+from pydicom import dcmread
+
+
+def get_acq_date(dcm_dir):
+    """ Parses dicom header to get the acquisition date
+    """
+    raw_dcm_set, invalid_dicom_list = search_dicoms(dcm_dir, skip_dcm_check=True)
+    # dcm_file = os.listdir(dcm_dir)[0]
+    dcm_path = list(raw_dcm_set)[0]
+    print(f"dcm_path: {dcm_path}")
+    # dcm_path = os.path.join(dcm_dir, dcm_file)
+
+    if check_valid_dicom(dcm_path):
+        with open(dcm_path, 'rb') as infile:
+            ds = dcmread(infile)
+
+        acq_date = ds.AcquisitionDate
+        acq_date = pd.to_datetime(acq_date, format="%Y%m%d").date().strftime('%Y-%m-%d')
+
+    else:
+        acq_date = None
+
+    return acq_date
+
+def run(global_configs, output_file, logger=None):
+    """ Run get_acq_date for all participants
+    """
+    # populate relative paths
+    DATASET_ROOT = global_configs["DATASET_ROOT"]
+    raw_dicom_dir = f"{DATASET_ROOT}/scratch/raw_dicom/"
+    log_dir = f"{DATASET_ROOT}/scratch/logs/"
+
+    fpath_doughnut = f"{DATASET_ROOT}/scratch/raw_dicom/{FNAME_DOUGHNUT}"
+    fpath_MRI_acqdata = f"{DATASET_ROOT}/scratch/raw_dicom/{output_file}"
+
+    df_doughnut = load_doughnut(fpath_doughnut)
+
+    if logger is None:
+        log_file = f"{log_dir}/mri_metadata.log"
+        logger = my_logger.get_logger(log_file)
+
+    logger.info("-"*50)
+    logger.info(f"Using DATASET_ROOT: {DATASET_ROOT}")
+
+    n_doughnut_participants = df_doughnut["participant_dicom_dir"].nunique()
+    n_doughnut_sessions = df_doughnut["session"].nunique()
+
+    logger.info(f"n_doughnut_participants: {n_doughnut_participants}")
+    logger.info(f"n_doughnut_sessions: {n_doughnut_sessions}")
+
+    if n_doughnut_participants > 0:
+        logger.info(f"Processing {n_doughnut_participants} participants")
+        useful_cols = ["participant_id","session","participant_dicom_dir"]
+        df_doughnut = df_doughnut[useful_cols].reset_index()
+
+        missing_mri_list = []
+        for idx, row in df_doughnut.iterrows():
+            participant_dicom_dir = row["participant_dicom_dir"]
+            session = row["session"]
+            dicom_path = f"{raw_dicom_dir}/{session}/{participant_dicom_dir}/"
+
+            try:
+                acq_date = get_acq_date(f"{dicom_path}")
+            except Exception as e:
+                acq_date = None
+                logger.error(f"get_acq_date call failed with exceptions: {e}")
+                missing_mri_list.append(participant_dicom_dir)
+
+            df_doughnut.loc[idx, "scanner_acq_date"] = acq_date
+
+        df_doughnut.to_csv(fpath_MRI_acqdata, index=False)
+
+        n_missing_mri = len(missing_mri_list)
+        logger.info(f"Could not find dicom acq date for {n_missing_mri} participants")
+        logger.info(f"Saved MRI acquisition data to: {fpath_MRI_acqdata}")
+
+if __name__ == '__main__':
+    # argparse
+    HELPTEXT = """
+    Script to get the acquisition date for all MRI scans in a session
+    """
+    parser = argparse.ArgumentParser(description=HELPTEXT)
+    parser.add_argument('--global_config', type=str, help='path to global config file for your nipoppy dataset', required=True)
+    parser.add_argument('--output_file', type=str, help='output file name', default="MRI_acqdata.csv") #TODO merge this with doughtnut
+    args = parser.parse_args()
+
+    # read global configs
+    global_config_file = args.global_config
+    with open(global_config_file, 'r') as f:
+        global_configs = json.load(f)
+
+    output_file = args.output_file
+
+    run(global_configs, output_file)
---
+++ imaging/extraction/freesurfer/run_FS_utils.py
@@ -0,0 +1,157 @@
+import argparse
+import json
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+import pandas as pd
+
+# Make sure FS_LICENSE is defined in the container.
+os.environ['SINGULARITYENV_FS_LICENSE'] = "/fsdir/license.txt"
+os.environ['SINGULARITYENV_SUBJECTS_DIR'] = "/fsdir/"
+
+
+def get_mris_preproc_cmd(FS_dir, participant_id, out_file_suffix, meas="thickness", fwhm=0, template="fsaverage"):
+    """ A function to generate FreeSurfer's mris_preproc command
+    """
+
+    fpath_lh = Path(f"{FS_dir}/{participant_id}/surf/lh.thickness")
+    fpath_rh = Path(f"{FS_dir}/{participant_id}/surf/rh.thickness")
+    fpath_status = Path.is_file(fpath_lh) & Path.is_file(fpath_rh)
+    FS_CMD_dict = {}
+
+    if fpath_status:
+        participants_str = f"--s {participant_id}"
+
+        for hemi in ["lh", "rh"]:
+            d = os.path.dirname(out_file_suffix)
+            f = os.path.basename(out_file_suffix)
+            hemi_out_file = f"{d}/{participant_id}_{hemi}_{f}"
+
+            FS_CMD = f"mris_preproc {participants_str} --target {template} --hemi {hemi} --meas {meas} --fwhm {fwhm} --out {hemi_out_file}"
+            FS_CMD_dict[hemi] = FS_CMD
+
+    else:
+        print(f"ignoring {participant_id} with missing surf files...")
+
+    return FS_CMD_dict
+
+
+def run(FS_dir, participants_list, out_file_suffix, meas, fwhm, template):
+    """ function to execute FS container with mris_preproc command
+    """
+    for participant_id in participants_list:
+        FS_CMD_dict = get_mris_preproc_cmd(FS_dir, participant_id, out_file_suffix, meas, fwhm, template)
+
+        if FS_CMD_dict:
+            for hemi, FS_CMD in FS_CMD_dict.items():
+                print(f"hemisphere: {hemi}")
+                CMD_ARGS = SINGULARITY_CMD + FS_CMD
+
+                CMD = CMD_ARGS.split()
+
+                print("-"*25)
+                print(CMD_ARGS)
+                print("-"*25)
+
+                try:
+                    proc = subprocess.run(CMD)
+
+                except Exception as e:
+                    print(f"mris_preproc run failed with exceptions: {e}")
+
+                print("-"*30)
+                print("")
+
+if __name__ == '__main__':
+    # argparse
+    HELPTEXT = """
+    Script to perform DICOM to BIDS conversion using HeuDiConv
+    """
+    parser = argparse.ArgumentParser(description=HELPTEXT)
+    parser.add_argument('--global_config', type=str, help='path to global config file for your nipoppy dataset', required=True)
+    parser.add_argument('--session_id', type=str, help='session_id', required=True)
+    parser.add_argument('--output_dir', type=str, default=None, help='out_file path for the processed / aggregated output')
+    parser.add_argument('--meas', type=str, default="thickness", help='cortical measure')
+    parser.add_argument('--fwhm', type=int, default=10, help='smoothing kernel in mm')
+    parser.add_argument('--template', type=str, default="fsaverage", help='freesurfer template (fsaverage or fsaverage5)')
+
+    # TODO add overwrite flag
+
+    args = parser.parse_args()
+
+    global_config_file = args.global_config
+    session_id = args.session_id
+
+    output_dir = args.output_dir
+
+    meas = args.meas
+    fwhm = args.fwhm
+    template = args.template
+
+    session = f"ses-{session_id}"
+
+    # Read global config
+    with open(global_config_file, 'r') as f:
+        global_configs = json.load(f)
+
+    dataset_root = global_configs["DATASET_ROOT"]
+    CONTAINER_STORE = global_configs["CONTAINER_STORE"]
+    FS_VERSION = global_configs["PROC_PIPELINES"]["freesurfer"]["VERSION"]
+    FS_CONTAINER = global_configs["PROC_PIPELINES"]["freesurfer"]["CONTAINER"]
+    FS_CONTAINER = FS_CONTAINER.format(FS_VERSION)
+    FS_CONTAINER_PATH = f"{CONTAINER_STORE}{FS_CONTAINER}"
+
+    # Paths
+    FS_dir = f"{dataset_root}/derivatives/freesurfer/v{FS_VERSION}/output/{session}/"
+    FS_license = f"{FS_dir}/license.txt"
+
+    if output_dir is None:
+        output_dir = f"{dataset_root}/derivatives/freesurfer/v{FS_VERSION}/surfmaps/{session}/"
+
+    Path(f"{output_dir}").mkdir(parents=True, exist_ok=True)
+
+    # grab bids_ids
+    manifest = f"{dataset_root}/manifest.csv"
+    doughnut = f"{dataset_root}/scratch/raw_dicom/doughnut.csv"
+
+    # Singularity CMD
+    SINGULARITY_CMD=f"singularity exec -B {FS_dir}:/fsdir -B {output_dir}:/output_dir {FS_CONTAINER_PATH} "
+
+    # Read participant lists and filter by session and group
+    doughnut_df = pd.read_csv(doughnut)
+    doughnut_df = doughnut_df[doughnut_df["session"] == session]
+    n_bids = len(doughnut_df["bids_id"].unique())
+
+    print("")
+    print("-"*50)
+    print("Starting FS analysis...")
+    print("-"*50)
+
+    print(f"using session: {session}")
+    print(f"number of available BIDS participants: {n_bids}")
+
+    proc_participants = doughnut_df["bids_id"].unique() #.isin(group_participants)]["bids_id"].values
+    n_proc_participants = len(proc_participants)
+    print(f"number of proc particiants (bids with group info): {n_proc_participants}")
+
+    if n_proc_participants > 0:
+        print("")
+        print("-"*50)
+        print("Starting FS utils")
+
+        out_file = f"/output_dir/surf_{meas}_{fwhm}mm.mgh"
+
+        print("Running mris_preproc separately for each participant and for left and right hemisphere\n")
+        run(FS_dir, proc_participants, out_file, meas, fwhm, template)
+
+        print(" -"*30)
+        print("")
+
+        print("mris_preproc run complete")
+        print("-"*50)
+    else:
+        print("-"*50)
+        print("No participants found to process...")
+        print("-"*50)
